9d53ef6e08b33b89643a62ef72df04dc24953d39
Johan <jsbollll@gmail.com>
2023-03-16 17:59:22 +0100

    begun on using parsing

--- src/para.rs
+++ src/para.rs
@@ -1,26 +1,50 @@
 #![allow(non_snake_case)]
 #![allow(non_upper_case_globals)]
 
+
 use core::ffi::c_void;
 use std::mem::transmute;
+use std::fs::File;
+use std::io::BufReader;
+use std::io::Read;
 extern "C"
 {
     fn LoadLibraryA(lpLibFileName: *const u8) -> *const c_void;
     fn GetProcAddress(hModule: *const c_void, lpProcName: *const u8) -> *const c_void;
 }
 
-struct Lexer;
-type CreateLexerFromBnfT = fn(src: *const u8) -> *mut Lexer; 
-
-fn stub(src: *const u8) -> *mut Lexer
+struct ParseToken
 {
-    return 0 as *mut Lexer;
+    token_type: i64,
+    data: *const u8,
+    length: u32,
 }
 
-static mut create_lexer_from_bnf: CreateLexerFromBnfT = stub;
+struct Expr
+{
+    token: ParseToken,
+    expr_count: u32,
+    exprs: [*mut Expr],
+}
+
+type WriteParseTableFromBnfT = fn(buffer: *mut c_void, buffer_size: u32, src: *const u8) -> u32;
+type ParseBinT = fn(token_list: *mut ParseToken, token_count: u32, table: *mut u8, flags: u32, opt_tree_out: *mut*mut Expr) -> bool;
+
+
+static mut write_parse_table_from_bnf: WriteParseTableFromBnfT = |_, _, _,| 0;
+static mut parse_bin: ParseBinT = |_, _, _, _, _,| false;
+
 
 fn main()
 {
+    let mut bnf_src: String;
+    {
+        let file = File::open("./src/bnf.txt").unwrap();
+        let mut buf_reader = BufReader::new(file);
+        bnf_src = String::new();
+        buf_reader.read_to_string(&mut bnf_src).unwrap();
+    }
+
     unsafe
     {
         let ptg_dll = LoadLibraryA(b"./lib/ptg.dll\0".as_ptr());
@@ -31,18 +55,36 @@ fn main()
             return;  
         }
         {
-            let addr = GetProcAddress(ptg_dll, b"create_lexer_from_bnf\0".as_ptr());
+            let addr = GetProcAddress(ptg_dll, b"write_parse_table_from_bnf\0".as_ptr());
             if addr == 0 as *const c_void
             {
-                println!("Failed to load create_lexer_from_bnf");
+                println!("Failed to load write_parse_table_from_bnf");
                 return;
             }
-            create_lexer_from_bnf = transmute::<*const c_void, CreateLexerFromBnfT>(addr);
+            write_parse_table_from_bnf = transmute::<*const c_void, WriteParseTableFromBnfT>(addr);
+        }
+        {
+            let addr = GetProcAddress(ptg_dll, b"parse_bin\0".as_ptr());
+            if addr == 0 as *const c_void
+            {
+                println!("Failed to load parse_bin");
+                return;
+            }
+            parse_bin = transmute::<*const c_void, ParseBinT>(addr);
         }
 
     }
+    let mut table: [u8; 10000] = [0; 10000];
+    let table_size: u32 = unsafe {write_parse_table_from_bnf(&mut table as *mut u8 as *mut c_void, 5000, bnf_src.as_bytes().as_ptr())};
+    if table_size > 10000
+    {
+        println!("failed to read table {}", table_size);
+        return;
+    }
+    let mut expr: *mut Expr;
+    let success: bool = unsafe {parse_bin()}
 
-    let lexer = unsafe{create_lexer_from_bnf(b"test\0".as_ptr())};
+
 
     println!("hello world");
 }
