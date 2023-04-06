d5d380dd52db61236f96f955d3bc0f631eb1a4d8
Johan-su <jsbollll@gmail.com>
2023-04-06 19:45:45 +0200

    begun on eval tree

--- src/para.rs
+++ src/para.rs
@@ -1,10 +1,11 @@
 #![allow(non_snake_case)]
 #![allow(non_upper_case_globals)]
+#![allow(non_camel_case_types)]
 
 mod ptg_header;
 
 use core::ffi::c_void;
-use std::mem::transmute;
+use core::panic;
 use std::fs::File;
 use std::io::BufReader;
 use std::io::Read;
@@ -15,7 +16,7 @@ use crate::ptg_header::*;
 
 
 
-enum TokenType
+enum LR_Type
 {
     TokenPlus,
     TokenMinus,
@@ -27,63 +28,68 @@ enum TokenType
     TokenNumber,
     TokenId,
     TokenEnd,
+    ExprS,
+    ExpraltS,
+    ExprFuncDecl,
+    ExprVarDecl,
+    ExprE,
+    ExprFuncCall,
+    ExprVar,
 }
 
-fn i64_to_TokenType(token_type: i64) -> Result<TokenType, &'static str>
+fn i64_to_TokenType(token_type: i64) -> Result<LR_Type, &'static str>
 {
     match token_type
     {
-        0 => Ok(TokenType::TokenPlus),
-        1 => Ok(TokenType::TokenMinus),
-        2 => Ok(TokenType::TokenTimes),
-        3 => Ok(TokenType::TokenDivide),
-        4 => Ok(TokenType::TokenEquals),
-        5 => Ok(TokenType::TokenOpen),
-        6 => Ok(TokenType::TokenClose),
-        7 => Ok(TokenType::TokenNumber),
-        8 => Ok(TokenType::TokenId),
-        9 => Ok(TokenType::TokenEnd),
+        0 => Ok(LR_Type::TokenPlus),
+        1 => Ok(LR_Type::TokenMinus),
+        2 => Ok(LR_Type::TokenTimes),
+        3 => Ok(LR_Type::TokenDivide),
+        4 => Ok(LR_Type::TokenEquals),
+        5 => Ok(LR_Type::TokenOpen),
+        6 => Ok(LR_Type::TokenClose),
+        7 => Ok(LR_Type::TokenNumber),
+        8 => Ok(LR_Type::TokenId),
+        9 => Ok(LR_Type::TokenEnd),
+        10 => Ok(LR_Type::ExprS),
+        11 => Ok(LR_Type::ExpraltS),
+        12 => Ok(LR_Type::ExprFuncDecl),
+        13 => Ok(LR_Type::ExprVarDecl),
+        14 => Ok(LR_Type::ExprE),
+        15 => Ok(LR_Type::ExprFuncCall),
+        16 => Ok(LR_Type::ExprVar),
         _ => Err("Invalid token type"),
     }
 }
 
 
-fn eval_tree(expr: &Expr)
+unsafe fn eval_tree(expr: *const Expr) -> i64
 {
-    let mut expr_stack: [*const Expr; 1024] = [0 as *const Expr; 1024];
-    
-    match i64_to_TokenType(expr.token.token_type).unwrap() 
     {
-        TokenType::TokenPlus => {
-
-        },
-        TokenType::TokenMinus => {
-
-        },
-        TokenType::TokenTimes => {
-
-        },
-        TokenType::TokenDivide => {
-
-        },
-        TokenType::TokenEquals => {
-
-        },
-        TokenType::TokenOpen => {
-
-        },
-        TokenType::TokenClose => {
-
-        },
-        TokenType::TokenNumber => {
-
-        },
-        TokenType::TokenId => {
-
-        },
-        TokenType::TokenEnd => {
-
-        }, 
+        match i64_to_TokenType((*expr).token.token_type).unwrap() 
+        {
+            LR_Type::TokenPlus => {
+                assert_eq!((*expr).expr_count, 3);
+                return (*expr).exprs[0] + (*expr).exprs[2]; 
+            }
+            LR_Type::TokenMinus => {}
+            LR_Type::TokenTimes => {}
+            LR_Type::TokenDivide => {}
+            LR_Type::TokenEquals => {}
+            LR_Type::TokenOpen => {}
+            LR_Type::TokenClose => {}
+            LR_Type::TokenNumber => {}
+            LR_Type::TokenId => {}
+            LR_Type::TokenEnd => {panic!("unreachable")}
+            LR_Type::ExprS => {panic!("unreachable")}
+            LR_Type::ExpraltS => {}
+            LR_Type::ExprFuncDecl => {}
+            LR_Type::ExprVarDecl => {}
+            LR_Type::ExprE => {}
+            LR_Type::ExprFuncCall => {}
+            LR_Type::ExprVar => {}
+        
+        }
     }
 }
 
@@ -111,7 +117,7 @@ fn main()
                 {
                     count += 1;
                 }
-                token_list.push(ParseToken {token_type: TokenType::TokenId as i64, data: &(arg[i] as i8), length: count as u32});
+                token_list.push(ParseToken {token_type: LR_Type::TokenId as i64, data: &(arg[i] as i8), length: count as u32});
                 i += count as usize - 1;
             }
             else if arg[i].is_ascii_alphanumeric()
@@ -121,26 +127,26 @@ fn main()
                 {
                     count += 1;
                 }
-                token_list.push(ParseToken {token_type: TokenType::TokenNumber as i64, data: &(arg[i] as i8), length: count as u32});
+                token_list.push(ParseToken {token_type: LR_Type::TokenNumber as i64, data: &(arg[i] as i8), length: count as u32});
                 i += count as usize - 1;
             }
             else
             {
                 match arg[i]
                 {
-                    b'+' => token_list.push(ParseToken {token_type: TokenType::TokenPlus as i64, data: &(arg[i] as i8), length: 1}),
-                    b'-' => token_list.push(ParseToken {token_type: TokenType::TokenMinus as i64, data: &(arg[i] as i8), length: 1}),
-                    b'*' => token_list.push(ParseToken {token_type: TokenType::TokenTimes as i64, data: &(arg[i] as i8), length: 1}),
-                    b'/' => token_list.push(ParseToken {token_type: TokenType::TokenDivide as i64, data: &(arg[i] as i8), length: 1}),
-                    b'=' => token_list.push(ParseToken {token_type: TokenType::TokenEquals as i64, data: &(arg[i] as i8), length: 1}),
-                    b'(' => token_list.push(ParseToken {token_type: TokenType::TokenOpen as i64, data: &(arg[i] as i8), length: 1}),
-                    b')' => token_list.push(ParseToken {token_type: TokenType::TokenClose as i64, data: &(arg[i] as i8), length: 1}),
+                    b'+' => token_list.push(ParseToken {token_type: LR_Type::TokenPlus as i64, data: &(arg[i] as i8), length: 1}),
+                    b'-' => token_list.push(ParseToken {token_type: LR_Type::TokenMinus as i64, data: &(arg[i] as i8), length: 1}),
+                    b'*' => token_list.push(ParseToken {token_type: LR_Type::TokenTimes as i64, data: &(arg[i] as i8), length: 1}),
+                    b'/' => token_list.push(ParseToken {token_type: LR_Type::TokenDivide as i64, data: &(arg[i] as i8), length: 1}),
+                    b'=' => token_list.push(ParseToken {token_type: LR_Type::TokenEquals as i64, data: &(arg[i] as i8), length: 1}),
+                    b'(' => token_list.push(ParseToken {token_type: LR_Type::TokenOpen as i64, data: &(arg[i] as i8), length: 1}),
+                    b')' => token_list.push(ParseToken {token_type: LR_Type::TokenClose as i64, data: &(arg[i] as i8), length: 1}),
                     _ => {eprintln!("ERROR: Unknown value {}", arg[i]); exit(1);},
                 }
             }
             i += 1;
         }
-        token_list.push(ParseToken {token_type: TokenType::TokenEnd as i64, data: &(arg[0] as i8), length: 0});
+        token_list.push(ParseToken {token_type: LR_Type::TokenEnd as i64, data: &(arg[0] as i8), length: 0});
     }
     let token_count = token_list.len() as u32;
 
@@ -167,7 +173,7 @@ fn main()
 
     let mut expr: *mut Expr = 0 as *mut Expr;
     let mut msg: [u8; 1000] = [0; 1000];
-    let success: bool = unsafe {parse_bin(token_list.as_mut_ptr(), token_count, table.as_mut_ptr(), 1, &mut expr, msg.as_mut_ptr() as *mut i8, 1000)};
+    let success: bool = unsafe {parse_bin(token_list.as_mut_ptr(), token_count, table.as_mut_ptr(), PRINT_EVERY_PARSE_STEP, &mut expr, msg.as_mut_ptr() as *mut i8, 1000)};
 
     
 
