feb1c0f5ef019b78a59bbf8dc1e5acca8469fe04
Johan-su <jsbollll@gmail.com>
2023-04-07 18:20:04 +0200

    eval working

--- src/para.rs
+++ src/para.rs
@@ -5,6 +5,7 @@
 mod ptg_header;
 
 use core::ffi::c_void;
+use core::num;
 use core::panic;
 use std::fs::File;
 use std::io::BufReader;
@@ -12,7 +13,6 @@ use std::io::Read;
 use std::env;
 use std::mem::transmute;
 use std::process::exit;
-use std::result;
 
 use crate::ptg_header::*;
 
@@ -40,7 +40,6 @@ enum LR_Type
 
 fn i64_to_TokenType(token_type: i64) -> Result<LR_Type, &'static str>
 {
-    dbg!(token_type);
     match token_type
     {
         0 => Ok(LR_Type::TokenPlus),
@@ -66,28 +65,38 @@ fn i64_to_TokenType(token_type: i64) -> Result<LR_Type, &'static str>
 unsafe fn parse_number(data: *const i8, data_length: u32) -> i64
 {
     let mut result: i64 = 0;
-
+    let mut num_pos: i64 = 1;
     for i in (0..data_length).rev()
     {
         let val = *(data.offset(i.try_into().unwrap())) as u8 as char; 
         match val
         {
-            '0' => {result += 0 * i as i64}
-            '1' => {result += 1 * i as i64}
-            '2' => {result += 2 * i as i64}
-            '3' => {result += 3 * i as i64}
-            '4' => {result += 4 * i as i64}
-            '5' => {result += 5 * i as i64}
-            '6' => {result += 6 * i as i64}
-            '7' => {result += 7 * i as i64}
-            '8' => {result += 8 * i as i64}
-            '9' => {result += 9 * i as i64}
+            '0' => {result += 0 * num_pos}
+            '1' => {result += 1 * num_pos}
+            '2' => {result += 2 * num_pos}
+            '3' => {result += 3 * num_pos}
+            '4' => {result += 4 * num_pos}
+            '5' => {result += 5 * num_pos}
+            '6' => {result += 6 * num_pos}
+            '7' => {result += 7 * num_pos}
+            '8' => {result += 8 * num_pos}
+            '9' => {result += 9 * num_pos}
             _ => {panic!("invalid number, got {}", val)}
         }
-
+        num_pos *= 10;
     }
     return result;
 }
+
+unsafe fn print_i8(data: *const i8, data_length: u32)
+{
+    for i in 0..data_length
+    {
+        print!("{}", (*(data.offset(i as isize)) as u8 as char));
+    }
+    println!();
+}
+
 
 
 unsafe fn get_expr_in_array(expr: *const Expr, index: isize) -> *const Expr
@@ -99,17 +108,13 @@ unsafe fn get_expr_in_array(expr: *const Expr, index: isize) -> *const Expr
 
 unsafe fn eval_tree(expr: *const Expr) -> i64
 {
-    dbg!(&*expr);
     match i64_to_TokenType((*expr).token.token_type).unwrap() 
     {
         LR_Type::TokenPlus => {panic!("unreachable")}
         LR_Type::TokenMinus => {panic!("unreachable")}
         LR_Type::TokenTimes => {panic!("unreachable")}
         LR_Type::TokenDivide => {panic!("unreachable")}
-        LR_Type::TokenEquals => {
-            assert_eq!((*expr).expr_count, 3);
-            return eval_tree(get_expr_in_array(expr, 2));
-        }
+        LR_Type::TokenEquals => {todo!("not implemented")}
         LR_Type::TokenOpen => {todo!("not implemented")}
         LR_Type::TokenClose => {todo!("not implemented")}
         LR_Type::TokenNumber => {return parse_number((*expr).token.data, (*expr).token.length)}
@@ -125,21 +130,44 @@ unsafe fn eval_tree(expr: *const Expr) -> i64
             match (*expr).expr_count
             {
                 1 => {return eval_tree(get_expr_in_array(expr, 0))}
-                2 => {todo!("not implemented")}
+                2 => {
+                    let token = i64_to_TokenType((*get_expr_in_array(expr, 1)).token.token_type).unwrap(); 
+                    match token 
+                    {
+                        LR_Type::TokenPlus => {return eval_tree(get_expr_in_array(expr, 0))}
+                        LR_Type::TokenMinus => {return -eval_tree(get_expr_in_array(expr, 0))}
+                        _ => panic!("unexpected token")          
+                    }
+                }
                 3 => {match i64_to_TokenType((*get_expr_in_array(expr, 1)).token.token_type).unwrap() 
                 {
                     LR_Type::TokenPlus => {
-                        return eval_tree(get_expr_in_array(expr, 0)) + eval_tree(get_expr_in_array(expr, 2));
+
+                        let num1 = eval_tree(get_expr_in_array(expr, 2)); 
+                        let num2 = eval_tree(get_expr_in_array(expr, 0)); 
+                        return num1 + num2;
                     }
                     LR_Type::TokenMinus => {
-                        return eval_tree(get_expr_in_array(expr, 0)) - eval_tree(get_expr_in_array(expr, 2));
+
+                        let num1 = eval_tree(get_expr_in_array(expr, 2)); 
+                        let num2 = eval_tree(get_expr_in_array(expr, 0)); 
+                        return num1 - num2;
                     }
                     LR_Type::TokenTimes => {
-                        return eval_tree(get_expr_in_array(expr, 0)) * eval_tree(get_expr_in_array(expr, 2));
+
+                        let num1 = eval_tree(get_expr_in_array(expr, 2)); 
+                        let num2 = eval_tree(get_expr_in_array(expr, 0)); 
+                        return num1 * num2;
                     }
                     LR_Type::TokenDivide => {
-                        return eval_tree(get_expr_in_array(expr, 0)) / eval_tree(get_expr_in_array(expr, 2));
-                    }                 
+
+                        let num1 = eval_tree(get_expr_in_array(expr, 2)); 
+                        let num2 = eval_tree(get_expr_in_array(expr, 0)); 
+                        return num1 / num2;
+                    }
+                    LR_Type::ExprE => {
+                        return eval_tree(get_expr_in_array(expr, 1));
+                    }
                     _ => panic!("unreachable")
                 }
 }
@@ -176,7 +204,7 @@ fn main()
                 {
                     count += 1;
                 }
-                token_list.push(ParseToken {token_type: LR_Type::TokenId as i64, data: &(arg[i] as i8), length: count as u32});
+                token_list.push(ParseToken {token_type: LR_Type::TokenId as i64, data: unsafe {arg.as_ptr().offset(i as isize) as *const i8}, length: count as u32});
                 i += count as usize - 1;
             }
             else if arg[i].is_ascii_alphanumeric()
@@ -186,21 +214,25 @@ fn main()
                 {
                     count += 1;
                 }
-                token_list.push(ParseToken {token_type: LR_Type::TokenNumber as i64, data: &(arg[i] as i8), length: count as u32});
+                token_list.push(ParseToken {token_type: LR_Type::TokenNumber as i64, data: unsafe {arg.as_ptr().offset(i as isize) as *const i8}, length: count as u32});
                 i += count as usize - 1;
             }
             else
             {
                 match arg[i]
                 {
-                    b'+' => token_list.push(ParseToken {token_type: LR_Type::TokenPlus as i64, data: &(arg[i] as i8), length: 1}),
-                    b'-' => token_list.push(ParseToken {token_type: LR_Type::TokenMinus as i64, data: &(arg[i] as i8), length: 1}),
-                    b'*' => token_list.push(ParseToken {token_type: LR_Type::TokenTimes as i64, data: &(arg[i] as i8), length: 1}),
-                    b'/' => token_list.push(ParseToken {token_type: LR_Type::TokenDivide as i64, data: &(arg[i] as i8), length: 1}),
-                    b'=' => token_list.push(ParseToken {token_type: LR_Type::TokenEquals as i64, data: &(arg[i] as i8), length: 1}),
-                    b'(' => token_list.push(ParseToken {token_type: LR_Type::TokenOpen as i64, data: &(arg[i] as i8), length: 1}),
-                    b')' => token_list.push(ParseToken {token_type: LR_Type::TokenClose as i64, data: &(arg[i] as i8), length: 1}),
-                    _ => {eprintln!("ERROR: Unknown value {}", arg[i]); exit(1);},
+                    b'+' => token_list.push(ParseToken {token_type: LR_Type::TokenPlus as i64, data: unsafe {arg.as_ptr().offset(i as isize) as *const i8}, length: 1}),
+                    b'-' => token_list.push(ParseToken {token_type: LR_Type::TokenMinus as i64, data: unsafe {arg.as_ptr().offset(i as isize) as *const i8}, length: 1}),
+                    b'*' => token_list.push(ParseToken {token_type: LR_Type::TokenTimes as i64, data: unsafe {arg.as_ptr().offset(i as isize) as *const i8}, length: 1}),
+                    b'/' => token_list.push(ParseToken {token_type: LR_Type::TokenDivide as i64, data: unsafe {arg.as_ptr().offset(i as isize) as *const i8}, length: 1}),
+                    b'=' => token_list.push(ParseToken {token_type: LR_Type::TokenEquals as i64, data: unsafe {arg.as_ptr().offset(i as isize) as *const i8}, length: 1}),
+                    b'(' => token_list.push(ParseToken {token_type: LR_Type::TokenOpen as i64, data: unsafe {arg.as_ptr().offset(i as isize) as *const i8}, length: 1}),
+                    b')' => token_list.push(ParseToken {token_type: LR_Type::TokenClose as i64, data: unsafe {arg.as_ptr().offset(i as isize) as *const i8}, length: 1}),
+                    b' ' => {}
+                    b'\n' => {}
+                    b'\r' => {}
+                    b'\t' => {}
+                    _ => {eprintln!("ERROR: Unknown char {}", arg[i] as char); exit(1);},
                 }
             }
             i += 1;
@@ -220,7 +252,8 @@ fn main()
     }
     bnf_src.push(0);
 
-    
+
+
     let mut table: [u8; 10000] = [0; 10000];
     let table_size: u32 = unsafe {write_parse_table_from_bnf(&mut table as *mut u8 as *mut c_void, 10000, bnf_src.as_ptr() as *const i8)};
     if table_size != 0
@@ -232,14 +265,19 @@ fn main()
 
     let mut expr: *mut Expr = 0 as *mut Expr;
     let mut msg: [u8; 1000] = [0; 1000];
-    let success: bool = unsafe {parse_bin(token_list.as_mut_ptr(), token_count, table.as_mut_ptr(), PRINT_EVERY_PARSE_STEP, &mut expr, msg.as_mut_ptr() as *mut i8, 1000)};
 
     
-
+    let success: bool = unsafe {parse_bin(token_list.as_mut_ptr(), token_count, table.as_mut_ptr(), /*PRINT_EVERY_PARSE_STEP*/0, &mut expr, msg.as_mut_ptr() as *mut i8, 1000)};
+    
+    
+    
     println!("success: {}, \n{}", success, std::str::from_utf8(&msg).unwrap());
-    unsafe {graphviz_from_syntax_tree(b"./input.dot\0".as_ptr() as *const i8, expr)};
+    if success
+    {
+        unsafe {graphviz_from_syntax_tree(b"./input.dot\0".as_ptr() as *const i8, expr)};
+        println!("evaled to {}", unsafe {eval_tree(expr)});
+    }
 
-    println!("evaled to {}", unsafe {eval_tree(expr)});
 
     unload_ptg();
 }
