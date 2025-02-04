eedf414c20d22a6b0e53fbbd4bb2bd31a6f19e44
Johan <jsbollll@gmail.com>
2025-02-04 22:56:01 +0100

    allow signed wrap and no strict aliasing

--- build.bat
+++ build.bat
@@ -1,7 +1,7 @@
 @echo off
 set CLANG=clang++
 set WARNINGS=-Wall -Wpedantic -Wextra -Wconversion -Wshadow -Wno-c++20-designator -Wno-c++17-extensions -Wno-gnu-anonymous-struct -Wno-nested-anon-types
-set FLAGS=-O0 -D _DEBUG -D _CRT_SECURE_NO_WARNINGS  -g %WARNINGS%
+set FLAGS=-O0 -D _CRT_SECURE_NO_WARNINGS -fwrapv -fno-strict-aliasing  -g %WARNINGS%
 
 set SANITIZE=-fsanitize=address -fsanitize=undefined
 set WINLIB= -l User32.lib -l Gdi32.lib -l Shell32.lib -l Winmm.lib
@@ -11,6 +11,6 @@ if not exist build mkdir build
 pushd build
 
 %CLANG% -E -Wno-macro-redefined ../src/meta.cpp > ../src/generated.cpp
-%CLANG% %WARNINGS% %FLAGS% -l ../lib/raylib.lib %WINLIB% -I ../lib/include  ../src/main.cpp -o para.exe
+%CLANG% %FLAGS% -l ../lib/raylib.lib %WINLIB% -I ../lib/include  ../src/main.cpp -o para.exe
 
 popd build
