00a2f93f5aa9ba883e17f4b618506bc69685c881
Johan <jsbollll@gmail.com>
2025-02-04 22:56:11 +0100

    new style

--- src/arena.cpp
+++ src/arena.cpp
@@ -1,32 +1,27 @@
-struct Arena
-{
+struct Arena {
     u64 pos;
     u64 max_capacity;
     u8 *data;
 };
 
 
-void init_arena(Arena *arena, u64 max_capacity)
-{
+void arena_init(Arena *arena, u64 max_capacity) {
     arena->pos = 0;
     arena->data = (u8 *)calloc(max_capacity, 1);
     arena->max_capacity = max_capacity;
 }
 
-void clean_arena(Arena *arena)
-{
+void arena_clean(Arena *arena) {
     free(arena->data);
     memset(arena, 0, sizeof(*arena));
 }
 
-u64 align_to_8_boundry(u64 a)
-{
-    usize offset = (8 - (a % 8)) % 8;
+u64 align_to_8_boundry(u64 a) {
+    u64 offset = (8 - (a % 8)) % 8;
     return a + offset;
 }
 
-void *alloc_arena(Arena *arena, u64 byte_amount)
-{
+void *arena_alloc(Arena *arena, u64 byte_amount) {
     assert(arena->pos % 8 == 0);
     if (arena->pos + byte_amount > arena->max_capacity)
     {
@@ -45,19 +40,16 @@ void *alloc_arena(Arena *arena, u64 byte_amount)
     return (void *)p;
 }
 
-void clear_arena(Arena *arena)
-{
+void arena_clear(Arena *arena) {
     arena->pos = 0;
 }
 
-u64 get_arena_pos(Arena *arena)
-{
+u64 arena_get_pos(Arena *arena) {
     assert(arena->pos % 8 == 0);
     return arena->pos;
 }
 
-void set_arena_pos(Arena *arena, u64 pos)
-{
+void arena_set_pos(Arena *arena, u64 pos) {
     assert(pos % 8 == 0);
     arena->pos = pos;
 }
