f1f5baedf9ab55cfa02b32e417307e21a8a381e6
Johan <jsbollll@gmail.com>
2025-06-15 23:12:39 +0200

    avoid windows.h conflicts in main

--- src/build.cpp
+++ src/build.cpp
@@ -2,8 +2,8 @@
 #include "meta.cpp"
 #include "common.cpp"
 #include "string.cpp"
-#include "window.cpp"
 #include "main.cpp"
+#include "window.cpp"
 
 #define NODE_INVALID WIN_NODE_INVALID
 #include <glad/glad.c>
